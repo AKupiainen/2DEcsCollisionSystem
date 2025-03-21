83775c526c3d4437b8ca930be1d233a7a5675e9d
Aki Kupiainen <kupiainen.aki@gmail.com>
2025-03-21 11:46:02 +0200

    Rewrote the the quadtree system

--- Assets/Systems/QuadTreeSystem.cs
+++ Assets/Systems/QuadTreeSystem.cs
@@ -6,10 +6,28 @@ using Unity.Mathematics;
 namespace ECSCollisionSystem
 {
     public struct NeedsSubdivisionTag : IComponentData { }
-
     public struct PendingInsertionComponent : IComponentData
     {
         public Entity TargetNode;
+    }
+
+    [BurstCompile]
+    public static class QuadtreeUtils
+    {
+        [BurstCompile]
+        public static bool IsAABBInsideNode(AABBComponent aabb, QuadtreeNodeComponent node)
+        {
+            return aabb.Min.x >= node.Position.x && aabb.Max.x <= node.Position.x + node.Size.x &&
+                   aabb.Min.y >= node.Position.y && aabb.Max.y <= node.Position.y + node.Size.y;
+        }
+
+        [BurstCompile]
+        public static float2 CalculateChildPosition(float2 parentPosition, float2 halfSize, int childIndex)
+        {
+            float xOffset = (childIndex % 2) * halfSize.x;
+            float yOffset = (childIndex / 2) * halfSize.y;
+            return parentPosition + new float2(xOffset, yOffset);
+        }
     }
 
     [BurstCompile]
@@ -18,62 +36,42 @@ namespace ECSCollisionSystem
         private EntityQuery _aabbQuery;
         private const int MaxEntitiesPerNode = 4;
 
-        protected override void OnCreate() { }
-
         protected override void OnUpdate()
         {
-            EntityCommandBuffer endSimulationECB = SystemAPI.GetSingleton<EndSimulationEntityCommandBufferSystem.Singleton>()
-                .CreateCommandBuffer(World.Unmanaged);
-            
+            EntityCommandBuffer ecb = SystemAPI.GetSingleton<EndSimulationEntityCommandBufferSystem.Singleton>().CreateCommandBuffer(World.Unmanaged);
             ComponentLookup<QuadtreeNodeComponent> quadtreeNodes = GetComponentLookup<QuadtreeNodeComponent>();
             BufferLookup<QuadtreeEntityBuffer> entityBuffers = GetBufferLookup<QuadtreeEntityBuffer>();
             
-            NativeArray<Entity> allNodes = SystemAPI.QueryBuilder().WithAll<QuadtreeNodeComponent>().Build().ToEntityArray(Allocator.Temp);
+            NativeList<Entity> allNodes = new(Allocator.Temp);
+            allNodes.CopyFrom(SystemAPI.QueryBuilder().WithAll<QuadtreeNodeComponent>().Build().ToEntityArray(Allocator.Temp));
 
             Entities.WithoutBurst().ForEach((Entity entity, ref AABBComponent aabb) =>
             {
-                InsertEntityIntoQuadtree(entity, aabb, quadtreeNodes, entityBuffers, allNodes, endSimulationECB);
+                foreach (Entity nodeEntity in allNodes)
+                {
+                    QuadtreeNodeComponent node = quadtreeNodes[nodeEntity];
+                    if (QuadtreeUtils.IsAABBInsideNode(aabb, node) && entityBuffers.HasBuffer(nodeEntity))
+                    {
+                        DynamicBuffer<QuadtreeEntityBuffer> buffer = entityBuffers[nodeEntity];
+                        if (buffer.Length < MaxEntitiesPerNode)
+                        {
+                            buffer.Add(new QuadtreeEntityBuffer { Entity = entity });
+                        }
+                        else
+                        {
+                            ecb.AddComponent<NeedsSubdivisionTag>(nodeEntity);
+                            ecb.AddComponent(entity, new PendingInsertionComponent { TargetNode = nodeEntity });
+                        }
+                        break;
+                    }
+                }
             }).Run();
 
             allNodes.Dispose();
         }
-
-        private void InsertEntityIntoQuadtree(Entity entity, AABBComponent aabb, 
-                                            ComponentLookup<QuadtreeNodeComponent> quadtreeNodes, 
-                                            BufferLookup<QuadtreeEntityBuffer> entityBuffers, 
-                                            NativeArray<Entity> nodeEntities,
-                                            EntityCommandBuffer ecb)
-        {
-            foreach (Entity nodeEntity in nodeEntities)
-            {
-                QuadtreeNodeComponent node = quadtreeNodes[nodeEntity];
-                
-                if (IsAABBInsideNode(aabb, node) && entityBuffers.HasBuffer(nodeEntity))
-                {
-                    DynamicBuffer<QuadtreeEntityBuffer> buffer = entityBuffers[nodeEntity];
-                    
-                    if (buffer.Length < MaxEntitiesPerNode)
-                    {
-                        buffer.Add(new QuadtreeEntityBuffer { Entity = entity });
-                    }
-                    else
-                    {
-                        ecb.AddComponent<NeedsSubdivisionTag>(nodeEntity);
-                        ecb.AddComponent(entity, new PendingInsertionComponent { TargetNode = nodeEntity });
-                    }
-                    break;
-                }
-            }
-        }
-
-        private bool IsAABBInsideNode(AABBComponent aabb, QuadtreeNodeComponent node)
-        {
-            return aabb.Min.x >= node.Position.x && aabb.Max.x <= node.Position.x + node.Size.x &&
-                   aabb.Min.y >= node.Position.y && aabb.Max.y <= node.Position.y + node.Size.y;
-        }
     }
 
-    [UpdateAfter(typeof(QuadtreeSystem))]
+    [BurstCompile, UpdateAfter(typeof(QuadtreeSystem))]
     public partial class QuadtreeSubdivisionSystem : SystemBase
     {
         private const int MaxEntitiesPerNode = 4;
@@ -86,95 +84,71 @@ namespace ECSCollisionSystem
 
         protected override void OnUpdate()
         {
-            EntityCommandBuffer ecb = SystemAPI.GetSingleton<EndSimulationEntityCommandBufferSystem.Singleton>()
-                .CreateCommandBuffer(World.Unmanaged);
-                
+            EntityCommandBuffer ecb = SystemAPI.GetSingleton<EndSimulationEntityCommandBufferSystem.Singleton>().CreateCommandBuffer(World.Unmanaged);
             ComponentLookup<QuadtreeNodeComponent> quadtreeNodes = GetComponentLookup<QuadtreeNodeComponent>();
             BufferLookup<QuadtreeEntityBuffer> entityBuffers = GetBufferLookup<QuadtreeEntityBuffer>();
             
+            NativeList<Entity> allNodes = new(Allocator.Temp);
+            allNodes.CopyFrom(_nodeQuery.ToEntityArray(Allocator.Temp));
+
             Entities.WithoutBurst().ForEach((Entity nodeEntity, in NeedsSubdivisionTag _) =>
             {
-                SubdivideNode(nodeEntity, quadtreeNodes, entityBuffers, ecb);
+                QuadtreeNodeComponent node = quadtreeNodes[nodeEntity];
+                float2 halfSize = node.Size / 2;
+                
+                for (int i = 0; i < 4; i++)
+                {
+                    Entity childNodeEntity = ecb.CreateEntity();
+                    ecb.AddComponent(childNodeEntity, new QuadtreeNodeComponent
+                    {
+                        Position = QuadtreeUtils.CalculateChildPosition(node.Position, halfSize, i),
+                        Size = halfSize
+                    });
+                    ecb.AddBuffer<QuadtreeEntityBuffer>(childNodeEntity);
+                }
+                
+                if (entityBuffers.HasBuffer(nodeEntity))
+                {
+                    DynamicBuffer<QuadtreeEntityBuffer> buffer = entityBuffers[nodeEntity];
+                    for (int i = 0; i < buffer.Length; i++)
+                    {
+                        ecb.AddComponent(buffer[i].Entity, new PendingInsertionComponent { TargetNode = nodeEntity });
+                    }
+                    buffer.Clear();
+                }
                 ecb.RemoveComponent<NeedsSubdivisionTag>(nodeEntity);
             }).Run();
-            
-            NativeArray<Entity> allNodes = _nodeQuery.ToEntityArray(Allocator.Temp);
-            
-            Entities.WithoutBurst().ForEach((Entity entity, in PendingInsertionComponent _, in AABBComponent aabb) =>
-            {
-                bool inserted = false;
 
-                foreach (var nodeEntity in allNodes)
+            Entities.WithoutBurst().ForEach((Entity entity, in PendingInsertionComponent pending, in AABBComponent aabb) =>
+            {
+                if (quadtreeNodes.HasComponent(pending.TargetNode) && entityBuffers.HasBuffer(pending.TargetNode))
                 {
-                    var node = quadtreeNodes[nodeEntity];
-                    
-                    if (IsAABBInsideNode(aabb, node) && entityBuffers.HasBuffer(nodeEntity))
+                    DynamicBuffer<QuadtreeEntityBuffer> buffer = entityBuffers[pending.TargetNode];
+                    if (buffer.Length < MaxEntitiesPerNode)
                     {
-                        var buffer = entityBuffers[nodeEntity];
-                        
+                        buffer.Add(new QuadtreeEntityBuffer { Entity = entity });
+                        ecb.RemoveComponent<PendingInsertionComponent>(entity);
+                        return;
+                    }
+                }
+
+                foreach (Entity nodeEntity in allNodes)
+                {
+                    QuadtreeNodeComponent node = quadtreeNodes[nodeEntity];
+                    if (QuadtreeUtils.IsAABBInsideNode(aabb, node) && entityBuffers.HasBuffer(nodeEntity))
+                    {
+                        DynamicBuffer<QuadtreeEntityBuffer> buffer = entityBuffers[nodeEntity];
                         if (buffer.Length < MaxEntitiesPerNode)
                         {
                             buffer.Add(new QuadtreeEntityBuffer { Entity = entity });
-                            inserted = true;
+                            ecb.RemoveComponent<PendingInsertionComponent>(entity);
                             break;
                         }
                     }
                 }
-                
-                if (inserted)
-                {
-                    ecb.RemoveComponent<PendingInsertionComponent>(entity);
-                }
-                
             }).Run();
-            
+
             allNodes.Dispose();
-        }
-        
-        private bool IsAABBInsideNode(AABBComponent aabb, QuadtreeNodeComponent node)
-        {
-            return aabb.Min.x >= node.Position.x && aabb.Max.x <= node.Position.x + node.Size.x &&
-                   aabb.Min.y >= node.Position.y && aabb.Max.y <= node.Position.y + node.Size.y;
-        }
-        
-        private void SubdivideNode(Entity nodeEntity, ComponentLookup<QuadtreeNodeComponent> quadtreeNodes, 
-                                  BufferLookup<QuadtreeEntityBuffer> entityBuffers, EntityCommandBuffer ecb)
-        {
-            QuadtreeNodeComponent node = quadtreeNodes[nodeEntity];
-            float2 halfSize = node.Size / 2;
-            
-            for (int i = 0; i < 4; i++)
-            {
-                QuadtreeNodeComponent childNode = new()
-                {
-                    Position = CalculateChildPosition(node.Position, halfSize, i),
-                    Size = halfSize
-                };
-
-                Entity childNodeEntity = ecb.CreateEntity();
-                ecb.AddComponent(childNodeEntity, childNode);
-                ecb.AddBuffer<QuadtreeEntityBuffer>(childNodeEntity);
-            }
-            
-            if (entityBuffers.HasBuffer(nodeEntity))
-            {
-                DynamicBuffer<QuadtreeEntityBuffer> buffer = entityBuffers[nodeEntity];
-                
-                for (int i = 0; i < buffer.Length; i++)
-                {
-                    ecb.AddComponent(buffer[i].Entity, new PendingInsertionComponent { TargetNode = nodeEntity });
-                }
-                
-                buffer.Clear();
-            }
-        }
-
-        private float2 CalculateChildPosition(float2 parentPosition, float2 halfSize, int childIndex)
-        {
-            float xOffset = childIndex % 2 == 0 ? 0 : halfSize.x;
-            float yOffset = childIndex < 2 ? 0 : halfSize.y;
-
-            return parentPosition + new float2(xOffset, yOffset);
         }
     }
 }
